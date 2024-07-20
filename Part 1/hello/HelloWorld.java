b329f52b3408f5546eca5713a6ea9b84bbbc5bc1
santheesh-14597 <santheesh.av@zohocorp.com>
2024-07-20 12:12:26 +0530

    HelloWorld

--- Part 1/hello/HelloWorld.java
+++ Part 1/hello/HelloWorld.java
@@ -4,9 +4,8 @@
  *  Last modified:     October 16, 1842
  **************************************************************************** */
 
-package PACKAGE_NAME;
 public class HelloWorld {
     public static void main(String[] args) {
-        
+        System.out.println("HelloWorld!");
     }
 }
