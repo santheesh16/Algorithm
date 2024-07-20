500276015737f40fdd2beabbb256c0878c6fc1b4
santheesh-14597 <santheesh.av@zohocorp.com>
2024-07-20 13:49:13 +0530

    8puzzle ass completed

--- Part 1/8puzzle/Board.java
+++ Part 1/8puzzle/Board.java
@@ -1,12 +1,118 @@
-/* *****************************************************************************
- *  Name:
- *  Date:
- *  Description:
- **************************************************************************** */
+import java.util.ArrayList;
+import java.util.Arrays;
 
-package PACKAGE_NAME;
 public class Board {
-    public static void main(String[] args) {
-        
+    private final int[][] tiles;
+    private final int n;
+
+    public Board(int[][] tiles) {
+        this.n = tiles.length;
+        this.tiles = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                this.tiles[i][j] = tiles[i][j];
+            }
+        }
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(n).append("\n");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                s.append(String.format("%2d ", tiles[i][j]));
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
+
+    public int dimension() {
+        return n;
+    }
+
+    public int hamming() {
+        int hamming = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (tiles[i][j] != 0 && tiles[i][j] != i * n + j + 1) {
+                    hamming++;
+                }
+            }
+        }
+        return hamming;
+    }
+
+    public int manhattan() {
+        int manhattan = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (tiles[i][j] != 0) {
+                    int goalRow = (tiles[i][j] - 1) / n;
+                    int goalCol = (tiles[i][j] - 1) % n;
+                    manhattan += Math.abs(i - goalRow) + Math.abs(j - goalCol);
+                }
+            }
+        }
+        return manhattan;
+    }
+
+    public boolean isGoal() {
+        return hamming() == 0;
+    }
+
+    public boolean equals(Object y) {
+        if (this == y) return true;
+        if (y == null || getClass() != y.getClass()) return false;
+        Board board = (Board) y;
+        return Arrays.deepEquals(tiles, board.tiles);
+    }
+
+    public Iterable<Board> neighbors() {
+        ArrayList<Board> neighbors = new ArrayList<>();
+        int blankRow = -1, blankCol = -1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (tiles[i][j] == 0) {
+                    blankRow = i;
+                    blankCol = j;
+                    break;
+                }
+            }
+        }
+
+        if (blankRow > 0)
+            neighbors.add(new Board(swap(blankRow, blankCol, blankRow - 1, blankCol)));
+        if (blankRow < n - 1)
+            neighbors.add(new Board(swap(blankRow, blankCol, blankRow + 1, blankCol)));
+        if (blankCol > 0)
+            neighbors.add(new Board(swap(blankRow, blankCol, blankRow, blankCol - 1)));
+        if (blankCol < n - 1)
+            neighbors.add(new Board(swap(blankRow, blankCol, blankRow, blankCol + 1)));
+
+        return neighbors;
+    }
+
+    public Board twin() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                if (tiles[i][j] != 0 && tiles[i][j + 1] != 0) {
+                    return new Board(swap(i, j, i, j + 1));
+                }
+            }
+        }
+        return null;
+    }
+
+    private int[][] swap(int row1, int col1, int row2, int col2) {
+        int[][] copy = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            copy[i] = Arrays.copyOf(tiles[i], n);
+        }
+        int temp = copy[row1][col1];
+        copy[row1][col1] = copy[row2][col2];
+        copy[row2][col2] = temp;
+        return copy;
     }
 }
--- Part 1/8puzzle/Solver.java
+++ Part 1/8puzzle/Solver.java
@@ -4,9 +4,88 @@
  *  Description:
  **************************************************************************** */
 
-package PACKAGE_NAME;
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.ArrayList;
+
 public class Solver {
-    public static void main(String[] args) {
-        
+    private final boolean solvable;
+    private final int moves;
+    private final Iterable<Board> solution;
+
+    private class SearchNode implements Comparable<SearchNode> {
+        private final Board board;
+        private final int moves;
+        private final SearchNode previous;
+        private final int priority;
+
+        public SearchNode(Board board, int moves, SearchNode previous) {
+            this.board = board;
+            this.moves = moves;
+            this.previous = previous;
+            this.priority = board.manhattan() + moves;
+        }
+
+        @Override
+        public int compareTo(SearchNode other) {
+            return Integer.compare(this.priority, other.priority);
+        }
+    }
+
+    public Solver(Board initial) {
+        if (initial == null) throw new IllegalArgumentException();
+
+        MinPQ<SearchNode> pq = new MinPQ<>();
+        pq.insert(new SearchNode(initial, 0, null));
+
+        MinPQ<SearchNode> twinPQ = new MinPQ<>();
+        twinPQ.insert(new SearchNode(initial.twin(), 0, null));
+
+        SearchNode goalNode = null;
+
+        while (true) {
+            goalNode = processQueue(pq);
+            if (goalNode != null || processQueue(twinPQ) != null) break;
+        }
+
+        if (goalNode == null) {
+            solvable = false;
+            moves = -1;
+            solution = null;
+        }
+        else {
+            solvable = true;
+            moves = goalNode.moves;
+            ArrayList<Board> path = new ArrayList<>();
+            for (SearchNode node = goalNode; node != null; node = node.previous) {
+                path.add(0, node.board);
+            }
+            solution = path;
+        }
+    }
+
+    private SearchNode processQueue(MinPQ<SearchNode> pq) {
+        if (pq.isEmpty()) return null;
+        SearchNode node = pq.delMin();
+        if (node.board.isGoal()) return node;
+
+        for (Board neighbor : node.board.neighbors()) {
+            if (node.previous == null || !neighbor.equals(node.previous.board)) {
+                pq.insert(new SearchNode(neighbor, node.moves + 1, node));
+            }
+        }
+        return null;
+    }
+
+    public boolean isSolvable() {
+        return solvable;
+    }
+
+    public int moves() {
+        return moves;
+    }
+
+    public Iterable<Board> solution() {
+        return solution;
     }
 }
