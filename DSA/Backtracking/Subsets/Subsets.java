908c02257d8474cf0a0e8f786b32631c4316e9a5
santheesh16 <santheesh16@gmail.com>
2025-06-22 22:51:57 +0530

    Backtracking | Subset

--- DSA/Backtracking/Subsets/Subsets.java
+++ DSA/Backtracking/Subsets/Subsets.java
@@ -1,9 +1,31 @@
 package Subsets;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Subsets {
 
-    public static void main(String[] args) {
+    public static List<List<Integer>> subsets(List<Integer> nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(0, new ArrayList<>(), nums, res);
+        return res;
+    }
 
+    private static void backtrack(int i, List<Integer> curr, List<Integer> nums, List<List<Integer>> res) {
+        if (i == nums.size()) {
+            res.add(new ArrayList<>(curr));
+            return;
+        }
+        curr.add(nums.get(i));
+        backtrack(i + 1, curr, nums, res);
+        curr.remove(curr.size() - 1);
+        backtrack(i + 1, curr, nums, res);
+    }
+
+    public static void main(String[] args) {
+        new Subsets();
+        System.out.println(Subsets.subsets(Arrays.asList(1, 2, 3)));
     }
 
 }
