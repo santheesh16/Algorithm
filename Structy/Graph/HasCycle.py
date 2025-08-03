e3dacdd8111407320743dc871ce347a1858c3076
santheesh16 <santheesh16@gmail.com>
2025-08-03 21:35:27 +0530

    graph structy

---
+++ Structy/Graph/HasCycle.py
@@ -0,0 +1,23 @@
+def has_cycle(graph):
+    visited = set()
+    for start_node in graph:
+        if cycle_detect(graph, start_node, set(), visited):
+            return True
+    return False
+
+def cycle_detect(graph, node, visiting, visited):
+    if node in visited:
+        return False
+
+    if node in visiting:
+        return True
+
+    visiting.add(node)
+
+    for neighbor in graph[node]:
+        if cycle_detect(graph, neighbor, visiting, visited):
+            return True
+
+    visiting.remove(node)
+    visited.add(node)
+    return False
